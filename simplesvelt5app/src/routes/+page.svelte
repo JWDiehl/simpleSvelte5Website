1e2eaacce3f094192e790215f394e44c3e0f2c84
JWDiehl <jwdiehl@example.com>
2025-09-29 12:10:09 -0400

    Update page.svelte with changes

--- simplesvelt5app/src/routes/+page.svelte
+++ simplesvelt5app/src/routes/+page.svelte
@@ -1,13 +1,19 @@
+<script>
+	let count = $state(0);
+    let b = 4;
+
+// function to increment the count
+
+    const whenBtnIsClicked = () => {
+
+</script>
+
+
 <div
 	class="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center"
 >
 	<div class="max-w-md mx-auto bg-white rounded-xl shadow-lg p-8 text-center">
 		<h1 class="text-4xl font-bold text-gray-800 mb-4">Hello</h1>
-		<a
-			href="https://svelte.dev/docs/kit"
-			class="inline-block bg-indigo-600 text-white px-6 py-3 rounded-lg hover:bg-indigo-700 transition-colors"
-		>
-			Read the Documentation
-		</a>
+		<button>text</button>
 	</div>
 </div>
